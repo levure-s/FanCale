9ac55693a798cd5f94521d8a2f937e2882d2b1de
levure-s <levure.sauvage@gmail.com>
2024-05-19 10:19:30 +0900

    check_loginInfo_when_back_from_mypage

--- lib/calender/components/mypage_button.dart
+++ lib/calender/components/mypage_button.dart
@@ -1,13 +1,17 @@
+import 'package:fancale/home/home_model.dart';
 import 'package:fancale/login/login_page.dart';
 import 'package:fancale/mypage/my_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MypageButton extends StatelessWidget {
   const MypageButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final home = context.watch<HomeModel>();
+
     return IconButton(
         onPressed: () async {
           if (FirebaseAuth.instance.currentUser != null) {
@@ -16,6 +20,7 @@ class MypageButton extends StatelessWidget {
                 MaterialPageRoute(
                     builder: (context) => const MyPage(),
                     fullscreenDialog: true));
+            home.checkLoginInfo();
           } else {
             await Navigator.push(
                 context,
